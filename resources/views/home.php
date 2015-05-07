5fbcc8a27e82d737391c3fbeb13ae55daefe21e8
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2015-05-07 15:11:00 +0700

    nggablmang

---
+++ resources/views/home.php
@@ -0,0 +1,182 @@
+<!doctype html>
+<html>
+<head>
+  <link rel="stylesheet" href="http://localhost/monkp/public/css/bootstrap.min.css">
+</head>
+<body style="background-color:white;overflow:auto !important;">
+    <div class="row bg-primary hidden-xs">
+		<div class="container" style="min-height:150px;">
+          <h3 style="line-height:150px;color:white;">[ SIMKP | Institut Teknologi Sepuluh Nopember ]</h3>
+		</div>
+	</div>
+	<div class="row bg-primary visible-xs">
+		<div class="container" style="min-height:150px;">
+          <h3 style="line-height:150px;color:white;padding-left:10px;">[ SIMKP | ITS ]</h3>
+
+		</div>
+	</div>
+<section id="content" class="col-lg-12 animated fadeInUp" style="padding-top:20px;height:500px">
+    <div class="container" >
+      <div class="row" >
+          <div class="col-lg-7 hidden-xs">
+                    <a class="navbar-brand block" href="#" style="text-align:left;">Pengumuman</a>
+          <section>
+              <div class="carousel slide auto panel-body" id="c-slide">
+                <ol class="carousel-indicators out">
+                                    <li data-target="#c-slide" data-slide-to="0" class="active"></li>
+                                    <li data-target="#c-slide" data-slide-to="1" class=""></li>
+                                    <li data-target="#c-slide" data-slide-to="2" class=""></li>
+                                    <li data-target="#c-slide" data-slide-to="3" class=""></li>
+                                    <!-- <li data-target="#c-slide" data-slide-to="1" class="active"></li> -->
+                </ol>
+                <div class="carousel-inner" style="padding-left:20px;padding-right:15px;">
+                                    <div class="item active" >
+                    <header>AKTIVASI AKUN T.INDUSTRI</header>
+                    <p>
+                    <small class="text-muted">Published March 18, 2015</small> <br>
+                    <em class="text-muted">
+                    <!-- <div class="thumb-lg" style="float:left;padding:5px 10px 5px 0;">
+                        <img src="" alt="">
+                    </div> -->
+                    <!-- <br /> -->
+                    <!-- Sosialisasi merupakan tahap yang sangat menentukan keberhasilan sebuah program layanan, khususnya layanan dalam bidang TIK (Teknologi Informasi dan Telekomunikasi). Dengan penyelenggaraan sosialisasi, pengguna akan mengetahui dengan lengkap layanan yang disediakan. Keinginan pengguna terhadap layanan TIK juga dapat ditangkap dengan lebih lengkap dalam proses sosialisasi layanan.  Dan program sosialisasi layanan telah menjadi salah satu tahapan wajib dalam pengembangan layanan TIK di ITS.</em> -->
+                    Aktivasi akun T.Industri dimulai pada 23 Maret 2015. Harap dipastikan email ITS telah aktif. Tks                    </em>
+                    </p>
+                    <div style="clear:both;"></div>
+                  </div>
+                                    <div class="item" >
+                    <header>SOSIALISASI SIM KP DI TEKNIK INDUSTRI</header>
+                    <p>
+                    <small class="text-muted">Published March 17, 2015</small> <br>
+                    <em class="text-muted">
+                    <!-- <div class="thumb-lg" style="float:left;padding:5px 10px 5px 0;">
+                        <img src="" alt="">
+                    </div> -->
+                    <!-- <br /> -->
+                    <!-- Sosialisasi merupakan tahap yang sangat menentukan keberhasilan sebuah program layanan, khususnya layanan dalam bidang TIK (Teknologi Informasi dan Telekomunikasi). Dengan penyelenggaraan sosialisasi, pengguna akan mengetahui dengan lengkap layanan yang disediakan. Keinginan pengguna terhadap layanan TIK juga dapat ditangkap dengan lebih lengkap dalam proses sosialisasi layanan.  Dan program sosialisasi layanan telah menjadi salah satu tahapan wajib dalam pengembangan layanan TIK di ITS.</em> -->
+                    Sosialisasi SIM KP akan diselenggarakan di Jurusan Teknik industri ITS pada tanggal 18 Maret 2018 pukul 15.30. Mahasiswa T.Industri yang akan melaksanakan KP diharapkan hadir. Tks                    </em>
+                    </p>
+                    <div style="clear:both;"></div>
+                  </div>
+                                    <div class="item" >
+                    <header>UJI COBA SIMKP</header>
+                    <p>
+                    <small class="text-muted">Published March 09, 2015</small> <br>
+                    <em class="text-muted">
+                    <!-- <div class="thumb-lg" style="float:left;padding:5px 10px 5px 0;">
+                        <img src="" alt="">
+                    </div> -->
+                    <!-- <br /> -->
+                    <!-- Sosialisasi merupakan tahap yang sangat menentukan keberhasilan sebuah program layanan, khususnya layanan dalam bidang TIK (Teknologi Informasi dan Telekomunikasi). Dengan penyelenggaraan sosialisasi, pengguna akan mengetahui dengan lengkap layanan yang disediakan. Keinginan pengguna terhadap layanan TIK juga dapat ditangkap dengan lebih lengkap dalam proses sosialisasi layanan.  Dan program sosialisasi layanan telah menjadi salah satu tahapan wajib dalam pengembangan layanan TIK di ITS.</em> -->
+                     Ujicoba SIMKP  akan dimulai tahun 2015 dan percobaan terbatas di Jurusan T.Industri, T.Informatika, dan T.Mesin. Selanjutnya akan diterapkan untuk di semua jurusan ITS. Semoga sukses dan membantu proses KP yang lebih cepat, bisa diakses dari luar kampus, transparan, dan mudah telusur. 
+                     </em>
+                    </p>
+                    <div style="clear:both;"></div>
+                  </div>
+                                    <div class="item" >
+                    <header>AKTIVASI AKUN SIMKP</header>
+                    <p>
+                    <small class="text-muted">Published March 02, 2015</small> <br>
+                    <em class="text-muted">
+                    <!-- <div class="thumb-lg" style="float:left;padding:5px 10px 5px 0;">
+                        <img src="" alt="">
+                    </div> -->
+                    <!-- <br /> -->
+                    <!-- Sosialisasi merupakan tahap yang sangat menentukan keberhasilan sebuah program layanan, khususnya layanan dalam bidang TIK (Teknologi Informasi dan Telekomunikasi). Dengan penyelenggaraan sosialisasi, pengguna akan mengetahui dengan lengkap layanan yang disediakan. Keinginan pengguna terhadap layanan TIK juga dapat ditangkap dengan lebih lengkap dalam proses sosialisasi layanan.  Dan program sosialisasi layanan telah menjadi salah satu tahapan wajib dalam pengembangan layanan TIK di ITS.</em> -->
+                     SIMKP akan digunakan untuk membantu proses pelaksanaan KP. Sgera aktifkan akun ada di SIMKP. Untuk mahasiswa TI, harap hubungi mbak Ima, di student center TI.  Email yang di bolehkan hanya menggunakan email ITS. Terimakasih                    </em>
+                    </p>
+                    <div style="clear:both;"></div>
+                  </div>
+                  
+                  <!-- <div class="item active">
+                    <header>DIHADAPAN MAHASISWA ITS, RISMA UNGKAPKAN MIMPI</header>
+                    <p>
+                      <small class="text-muted">Published Maret 07, 2014</small><br>
+
+                      <em class="text-muted">
+
+                      <div class="thumb-lg" style="float:left;padding:5px 10px 5px 0;">
+                        <img src="" alt="">
+                      </div>
+
+                      <br />
+                    Sosiali Gedung Graha Sepuluh Nopember lagi-lagi menjadi lautan biru mahasiswa ITS yang memadatinya, Kamis (06/2). Tak kurang dari 1300 mahasiswa berseragam almamater kampus perjuangan ini mengisi setiap sudut ruang di gedung tersebut. Ternyata, kehadiran Tri Rismaharini ST MT bersama grup band Slank yang menjadi alasannya. </em></p>
+                  </div> -->
+                </div>
+                                <a class="left carousel-control" href="#c-slide" data-slide="prev"> <i class="fa fa-angle-left"></i> </a> <a class="right carousel-control" href="#c-slide" data-slide="next"> <i class="fa fa-angle-right"></i> </a> </div>
+            </section>
+            </div>
+
+
+
+            <div class="panel col-sm-12 col-lg-4 col-lg-offset-1" style="overflow:hidden;">
+
+            <a class="navbar-brand block" href="#" style="text-align:left;">Sign In</a>
+            <section class="m-b-lg">
+              <header class="wrapper"> <strong>Login to access your account.</strong> </header>
+              <form action="http://simkp.its.ac.id/index.php/login/process" method="post">
+                <input type="hidden" name="csrf_token"
+                       value="b126c7fb53c4df99da8504627fb56c3f" />
+                <input type="hidden" name="task" value="1">
+                <div class="list-group">
+                  <div class="list-group-item">
+                    <input type="email" name="username" placeholder="Your E-mail" class="form-control no-border" autocomplete="off" required>
+                  </div>
+                  <div class="list-group-item" style="margin-top:5px;">
+                    <input type="password" name="password" placeholder="Your Password" class="form-control no-border" required>
+                  </div>
+                </div>
+
+                <button type="submit" class="btn btn-lg btn-primary">Sign In</button>
+                <!-- <h4 style="float:right;"><a href="#" class="text-success">Forgot Password ?</a></h4> -->
+                <div style="clear:both;"></div>
+                <div class="line line-dashed"></div>
+              </form>
+            </section>
+
+          </div>
+      </div>
+    </div>
+
+
+	<footer id="footer">
+	  <div class="row">
+		  <div class="container text-right" >
+		  <div class="btn-group">
+			  <a href="http://webmail.its.ac.id" class="hover" >
+				  <span class="i-s i-s-3x " data-toggle="tooltip" data-placement="bottom"  data-original-title="webmail.its.ac.id" >
+					  <i class="i i-hexagon2 i-s-base text-primary hover-rotate"></i> <i class="i  i-mail2 i-sm text-white"> </i>
+				  </span>
+			  </a>
+			  <a href="http://integra.its.ac.id" class="hover" >
+				  <span class="i-s i-s-3x "  data-toggle="tooltip" data-placement="bottom"  data-original-title="integra.its.ac.id" >
+					  <i class="i i-hexagon2 i-s-base text-primary hover-rotate"></i> <i class="i  i-users2 i-sm text-white"> </i>
+				  </span>
+			  </a>
+			  <a href="http://its.ac.id" class="hover">
+				  <span class="i-s i-s-3x "  data-toggle="tooltip" data-placement="bottom"  data-original-title="its.ac.id" >
+					  <i class="i i-hexagon2 i-s-base text-primary hover-rotate"></i> <i class="i i-home i-sm text-white"> </i>
+				  </span>
+			  </a>
+
+		  </div>
+		  </div>
+	  </div>
+
+	</footer>
+</section>
+
+
+
+
+<!-- Bootstrap -->
+<!-- App -->
+<script src="http://simkp.its.ac.id/assets/dev/js/jquery-1.10.2.min.js"></script>
+<script src="http://simkp.its.ac.id/assets/dev/js/app.v1.js"></script>
+<script type="text/javascript" src="http://simkp.its.ac.id/assets/dev/js/jquery.nicescroll.js"></script>
+<script type="text/javascript" >
+   // $("body").niceScroll({styler:"fb",cursorcolor:"#1d89cf", cursorwidth: '6', cursorborderradius: '10px', background: '#eaeef1',spacebarenabled:true,  cursorborder: '', zindex: '1000'});
+</script>
+
+</body>
+</html>
