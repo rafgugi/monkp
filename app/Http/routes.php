39d9c75180bed07cdc8f6e8ff692857b8dd8d89a
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2015-05-17 20:16:04 +0700

    pengajuan

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,6 +13,7 @@
 
 Route::get('/', 'WelcomeController@index');
 Route::get('home', 'HomeController@index');
+Route::get('pengajuan', 'PengajuanController@create');
 
 Route::controllers([
 	'auth' => 'AuthController',
