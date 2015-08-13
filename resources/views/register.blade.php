d69bdbd68eab3552f6b04e9b15dc320e0d265932
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2015-08-13 13:48:29 +0700

    ga penting

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -42,12 +42,6 @@
                 </div>
               </div>
               <div class="form-group">
-                <label class="col-md-4 control-label">E-Mail</label>
-                <div class="col-md-6">
-                  <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-                </div>
-              </div>
-              <div class="form-group">
                 <label class="col-md-4 control-label">Password</label>
                 <div class="col-md-6">
                   <input type="password" class="form-control" name="password">
